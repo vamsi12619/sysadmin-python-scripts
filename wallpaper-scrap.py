126fc46dc9f0f33fe5a1f06c5ad2af16deb5971a
NotYourGuy <lucianvasile0@gmail.com>
2019-10-27 08:45:15 +0200

    comments revisited

--- wallpaper-scrap.py
+++ wallpaper-scrap.py
@@ -17,10 +17,11 @@ hot_subreddit = reddit.subreddit('EarthPorn').top('month', limit=50)
 for post in hot_subreddit:
     url.append(post.url) # each link from each post is appended to a list
 
+# https://stackoverflow.com/a/3173388
 for value in url:
     name = os.path.basename(value) # taking only the value after '/' from the url as name
     
-    os.makedirs(os.path.dirname(DOWNLOADS_DIR), exist_ok=True) # makes the directory where the photos are saved if it doesn't exist
+    os.makedirs(os.path.dirname(DOWNLOADS_DIR), exist_ok=True) # makes the directory where the photos are saved if it doesn't exist https://stackoverflow.com/a/12517490
     
     filename = os.path.join(DOWNLOADS_DIR, name) # combine the name and the downloads directory to get the local filename
     
