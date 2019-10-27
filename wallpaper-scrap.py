94b11b9da4b6010b0a61b0df0cc6d00d75b63b59
NotYourGuy <lucianvasile0@gmail.com>
2019-10-27 08:43:12 +0200

    added comments to script

--- wallpaper-scrap.py
+++ wallpaper-scrap.py
@@ -2,6 +2,7 @@
 
 import praw, os, urllib
 
+# Initialize reddit using your credentials http://www.storybench.org/how-to-scrape-reddit-with-python/
 reddit = praw.Reddit(client_id='YOUR_CLIENT_ID',
                      client_secret='YOUR_CLIENT_SECRET',
                      user_agent='YOUR_APP_NAME',
@@ -14,14 +15,14 @@ url = []
 hot_subreddit = reddit.subreddit('EarthPorn').top('month', limit=50)
 
 for post in hot_subreddit:
-    url.append(post.url)
+    url.append(post.url) # each link from each post is appended to a list
 
 for value in url:
-    name = os.path.basename(value)
+    name = os.path.basename(value) # taking only the value after '/' from the url as name
     
-    os.makedirs(os.path.dirname(DOWNLOADS_DIR), exist_ok=True)
+    os.makedirs(os.path.dirname(DOWNLOADS_DIR), exist_ok=True) # makes the directory where the photos are saved if it doesn't exist
     
-    filename = os.path.join(DOWNLOADS_DIR, name)
+    filename = os.path.join(DOWNLOADS_DIR, name) # combine the name and the downloads directory to get the local filename
     
     if not os.path.isfile(filename):
-        urllib.request.urlretrieve(value, filename)
+        urllib.request.urlretrieve(value, filename) # if the file doesn't exist, it gets downloaded
